ed8e2a0bb06a0dcacb1f942725d77eb8a5302be5
Jalen.Jordan <jalenjordan55@yahoo.com>
2018-10-12 10:52:28 -0500

    first commit

---
+++ net_pay.js
@@ -0,0 +1,6 @@
+var output = document.getElementById('output')
+var grossPay = parseInt(document.getElementById('grossPay').value);
+var  fedTax = grossPay * .15;
+var stateTax = grossPay * .05;
+var ficaTax = grossPay * .0762;
+document.write(netPay);
